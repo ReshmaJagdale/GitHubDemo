0189788bc3f8577aab5146874f751384d7c13790
Reshma Jagdale <jagdale.reshma04@gmail.com>
2025-01-29 23:45:20 +0530

    Thank you both

--- src/test/java/Demo1/TestGit1.java
+++ src/test/java/Demo1/TestGit1.java
@@ -5,7 +5,7 @@ public class TestGit1 {
 	public static void main(String[] args) {
 		System.out.println("Test1 Push code");
 		System.out.println("Test pull code");
-
+		System.out.println("Successfully changes done");
 	}
 
 }
---
+++ src/test/java/Demo1/TestGit2.java
@@ -0,0 +1,10 @@
+package Demo1;
+
+public class TestGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Kiran and pranav");
+
+	}
+
+}
